63375b74af885559bed0b921fd154ce5ce292155
Amit Singh <cse.amitallenhouse@gmail.com>
2025-09-29 16:12:14 +0530

    meta XML file in lightning Web Components

--- force-app/main/default/lwc/displayAccountLWC/displayAccountLWC.js
+++ force-app/main/default/lwc/displayAccountLWC/displayAccountLWC.js
@@ -1,7 +1,3 @@
 import { LightningElement } from 'lwc';
 
-export default class DisplayAccountLWC extends LightningElement {
-    handleChange(event){
-        console.log(event);
-    }
-}
+export default class DisplayAccountLWC extends LightningElement {}
